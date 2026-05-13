481335cb81bdcb0d3ffa5eebc2c67ddce16adcf9
Tin Phan Laptop <linhtrang050592@gmail.com>
2026-05-13 11:54:11 +0700

    refactor: update Google Sign-In button implementation to use plugin-provided render method

--- lib/widgets/common/google_button_web.dart
+++ lib/widgets/common/google_button_web.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in_platform_interface/google_sign_in_platform_interface.dart';
 import 'package:google_sign_in_web/google_sign_in_web.dart' as web;
 
 Widget buildWebGoogleButton() {
-  return web.renderButton(
-    configuration: web.GsiButtonConfiguration(
-      type: web.GsiButtonType.standard,
-      theme: web.GsiButtonTheme.outline,
-      size: web.GsiButtonSize.large,
-      shape: web.GsiButtonShape.rectangular,
-      text: web.GsiButtonText.continueWith,
-    ),
-  );
+  final web.GoogleSignInPlugin googleSignInPlugin = 
+      GoogleSignInPlatform.instance as web.GoogleSignInPlugin;
+  return googleSignInPlugin.renderButton();
 }
